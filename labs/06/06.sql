996d81de67ba15e8690881560efd4d9fd4824a00
coalpha <22552895+coalpha@users.noreply.github.com>
2020-11-10 06:13:23 -0800

    lab6: done???
    I hope this is all that he wanted.

--- labs/06/06.sql
+++ labs/06/06.sql
@@ -1,18 +1,13 @@
+set linesize 140;
+set pagesize 40;
+
 /*
 Dean of Admission at DeAnza College has hired you to do the database design for
 a new registration system. Keeping in the mind the following requirements,
 design a Database Schema.
 */
 
-6.
-Students have to pay for the course they take. You may want to think about
-resident, non resident.
-
-7.
-Their status is updated based on their registration. A document to help you
-remind the symbols for DB Schema is posted in the notes area for Session 1 -
-called Introduction to RDBMS.doc.
-*/
+-- the suffering begins now
 
 drop table user_6 cascade constraints;
 drop table location_6 cascade constraints;
@@ -21,7 +16,9 @@ drop table resident_6 cascade constraints;
 drop table college_6 cascade constraints;
 drop table department_6 cascade constraints;
 drop table course_6 cascade constraints;
+drop table course_prereq_join_6 cascade constraints;
 drop table section_6 cascade constraints;
+drop table course_user_join_6 cascade constraints;
 
 create table user_6 (
    id number(8) primary key,
@@ -77,7 +74,7 @@ create table course_prereq_join_6 (
    course_id number(5) references course_6(id),
    prereq_id number(5) references course_6(id),
    -- enforce the uniqueness
-   constraint primary key course_prereq_join_pk (course_id, prereq_id)
+   constraint course_prereq_join_pk primary key (course_id, prereq_id)
 );
 
 create table section_6 (
@@ -85,7 +82,7 @@ create table section_6 (
    -- we can assume that the section number does not change
    id number(5) primary key,
    -- 3. Courses have sections.
-   course_id number(6) references course_6(id),
+   course_id number(5) references course_6(id),
    location_id number(4) references location_6(id),
    instructor_id number(8) references user_6(id),
    start_time number(4), -- military time like 1832 or 0830
@@ -103,11 +100,9 @@ create table section_6 (
 create table course_user_join_6 (
    course_id number(5) references course_6(id),
    user_id number(8) references user_6(id),
-
    -- this is just to keep track of which sections people are enrolled in
    section_id number(5) references section_6(id),
-
-   constraint primary key course_user_join_pk (course_id, user_id)
+   constraint course_user_join_pk primary key (course_id, user_id)
 );
 
 insert into user_6
@@ -119,7 +114,8 @@ union all select 4, 'Cafra', 'Satz', null, null, null from dual
 union all select 5, 'Mr. Sun', 'Microsystems', null, null, null from dual
 union all select 6, 'BEN', 'STEED', null, null, null from dual
 union all select 7, 'Linux', 'Sebastian', null, null, null from dual
-union all select 8, 'Cassandra', 'Deebee', null, null, null from dual;
+union all select 8, 'Cassandra', 'Deebee', null, null, null from dual
+union all select 9, 'Panser', 'Bjarne', null, null, null from dual;
 
 insert into payment_type_6 (id, type)
 select 0, 'cash' from dual
@@ -190,7 +186,7 @@ insert into department_6 (college_id, id, name)
 select 0, 0, 'CIS Department' from dual
 union all select 0, 1, 'Ultimate Anthropology Department' from dual
 union all select 1, 2, 'East West Studies' from dual
-union all select 3, 2, 'Department of Human Suffering and Labor' from dual;
+union all select 3, 3, 'Department of Human Suffering and Labor' from dual;
 
 insert into course_6 (dept_id, id, name)
 select 0, 0, 'Visual Basic .NET Programming I' from dual
@@ -200,9 +196,10 @@ union all select 0, 3, 'Intro To SQL' from dual
 union all select 3, 4, 'Flensing for fun and profit!' from dual;
 
 insert into section_6 (id, course_id, location_id, instructor_id)
-select 25934, 3, 2, 7 from dual
+select 25934, 3, 2, 8 from dual
 union all select 23382, 2, 1, 5 from dual
-union all select 21718, 1, 0, 6 from dual;
+union all select 21718, 1, 0, 9 from dual
+union all select 00666, 4, 18, 6 from dual;
 
 -- let's enroll me into some of my courses
 
@@ -217,3 +214,70 @@ Students register for a section from a catalog (which is composed of
 Departments, Courses, Sections, Location.)
 */
 
+set wra off;
+column "SCTN#" format 99999;
+column "Course Name" format A32;
+column "Department" format A16;
+column "Location" format A23;
+
+select
+   sctn.id as "SCTN#",
+   crse.name as "Course Name",
+   dept.name as "Department",
+   loc.name as "Location"
+from section_6 sctn
+left join location_6 loc
+on sctn.location_id = loc.id
+left join course_6 crse
+on sctn.course_id = crse.id
+left join department_6 dept
+on crse.dept_id = dept.id;
+
+/*
+ SCTN# Course Name                      Department       Location
+------ -------------------------------- ---------------- -----------------------
+ 25934 Intro To SQL                     CIS Department   Student Council Dungeon
+ 23382 Java Programming                 CIS Department   Admin
+ 21718 Beginning Programming Methodolog CIS Department   Flint and Steel Center
+   666 Flensing for fun and profit!     Department of Hu Call Room 0
+*******************************************************************************/
+
+-- just for fun, since I'm actually kinda rockin' this db stuff
+-- let's select a list of each section and who teaches it
+
+column "Instuctor" format A16;
+
+select
+   sctn.id as "SCTN#",
+   crse.name as "Course Name",
+   'Prof. ' || usr.last_name as "Instructor"
+from section_6 sctn
+join course_6 crse
+on sctn.course_id = crse.id
+join user_6 usr
+on sctn.instructor_id = usr.id;
+
+/*
+ SCTN# Course Name                      Instructor
+------ -------------------------------- --------------------------------------
+ 23382 Java Programming                 Prof. Microsystems
+   666 Flensing for fun and profit!     Prof. STEED
+ 25934 Intro To SQL                     Prof. Sebastian
+ 21718 Beginning Programming Methodolog Prof. Bjarne
+*******************************************************************************/
+/*
+6.
+Students have to pay for the course they take. You may want to think about
+resident, non resident.
+*/
+
+-- and it was about around here that I realized that we didn't actually have to
+-- make this for lab6 and I didn't read the instructions correctly.
+-- 
+
+/*
+7.
+Their status is updated based on their registration. A document to help you
+remind the symbols for DB Schema is posted in the notes area for Session 1 -
+called Introduction to RDBMS.doc.
+*/
